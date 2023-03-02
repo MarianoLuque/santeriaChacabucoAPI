041816fd801286a3ed650a650d6371d68ebffc18
Nano <mn.luque.14@gmail.com>
2023-03-02 17:12:52 -0300

    tipo de visualizacion

--- models/atributos.js
+++ models/atributos.js
@@ -4,7 +4,7 @@ const AtributesScheme = new mongoose.Schema(
     {
         value: {type:String, required: true},
         description: {type:String, required: false},
-        type: {type: [mongoose.Types.ObjectId], ref: 'atributosTypes', required: true}
+        type: {type: mongoose.Types.ObjectId, ref: 'atributosTypes', required: true}
     },
     {
         timestamps: true,
